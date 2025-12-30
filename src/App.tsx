66bc2410303ac4fc2c290c14eafbfb7bd211672f
Rishabh Sinha <rishabhsinha220@gmail.com>
2025-12-29 21:56:10 -0500

    Adding analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import BuilderPage from './pages/BuilderPage'
+import { Analytics } from '@vercel/analytics/react';
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/builder" element={<BuilderPage />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   )
 }
